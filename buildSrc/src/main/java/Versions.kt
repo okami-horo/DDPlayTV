8f40a67ab301920c8987d4cbbc3a8e596119198b
202130310192@stu.shmtu.edu.cn <202130310192@stu.shmtu.edu.cn>
2025-11-12 03:08:46 +0000

    build: 升级Android SDK版本到35

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,8 +1,8 @@
 object Versions {
     const val applicationId = "com.xyoye.dandanplay"
-    const val compileSdkVersion = 34
+    const val compileSdkVersion = 35
     const val minSdkVersion = 21
-    const val targetSdkVersion = 34
+    const val targetSdkVersion = 35
     const val versionCode = 59
     const val versionName = "4.1.2"
 }
